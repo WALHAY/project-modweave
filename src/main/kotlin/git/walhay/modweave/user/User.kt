22c145f0774d2592fe2fa886ecb977a4d821e9f9
WALHAY <walhayggwp@gmail.com>
2026-02-16 16:57:28 +0300

    Change user to meet db attributes

--- src/main/kotlin/git/walhay/modweave/user/User.kt
+++ src/main/kotlin/git/walhay/modweave/user/User.kt
@@ -3,18 +3,14 @@ package git.walhay.modweave.user
 import com.fasterxml.jackson.annotation.JsonBackReference
 import com.fasterxml.jackson.annotation.JsonManagedReference
 import git.walhay.modweave.mods.Mod
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.OneToMany
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import java.sql.Date
 
 @Entity(name = "users")
 @Table(schema = "modweave", name = "users")
 data class User(
     @Id @Column(name = "login") val login: String,
-    @Column(name = "nickname", unique = true, nullable = false) val username: String,
+    @Column(name = "username", unique = true, nullable = false) val username: String,
     @Column(name = "email", unique = true, nullable = false) val email: String,
     @Column(name = "passhash", nullable = false) @JsonBackReference val passhash: String,
     @Column(name = "register_date", nullable = false) val register_date: Date,
@@ -24,4 +20,11 @@ data class User(
     val mods: MutableSet<Mod> = mutableSetOf()
 ) {
   constructor() : this("", "", "", "", Date(System.currentTimeMillis()), false)
+
+  constructor(
+      login: String,
+      username: String,
+      email: String,
+      passhash: String
+  ) : this(login, username, email, passhash, Date(System.currentTimeMillis()), false)
 }
