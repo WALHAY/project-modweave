16b00dfb0ac20f477f226f33bd5a9e34cd5f1069
WALHAY <walhayggwp@gmail.com>
2026-05-29 14:19:42 +0300

    Comment serialization

--- business-logic/src/main/kotlin/git/walhay/modweave/api/comment/CommentId.kt
+++ business-logic/src/main/kotlin/git/walhay/modweave/api/comment/CommentId.kt
@@ -1,8 +1,10 @@
 package git.walhay.modweave.api.comment
 
+import java.io.Serializable
+
 @JvmInline
 value class CommentId(
     val value: Long = 0,
-) {
+) : Serializable {
   override fun toString(): String = value.toString()
 }
